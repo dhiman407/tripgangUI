90d108c917c6af9b47cac789b73ee0f5927d8eea
Varun Kashyap <varunkashyapcse@gmail.com>
2014-08-20 18:48:21 -0700

    Changes in sass tool chain to include custom.css
    Make all the style changes in custom.css, this would then be picked up by the build and
    make it into the final apk

--- app/view/CreateTrip/TripType.js
+++ app/view/CreateTrip/TripType.js
@@ -1,12 +1,13 @@
 Ext.define('tripgang.view.CreateTrip.TripType', {
-    extend: 'Ext.dataview.List',
+    extend: 'Ext.dataview.DataView',
     xtype: 'tripType',
 	id:'tripsList',
     config: {
 		store: 'TripTypes',
         emptyText: 'Loading list of Type of Trips...',
         itemTpl: [
-            '<div class="legislator-list-item">',
+            '<div class="trip-type-item">',
+                '<span class="trip-type-icon">&nbsp;</span>',
                 '{name}',
             '</div>'
         ]
